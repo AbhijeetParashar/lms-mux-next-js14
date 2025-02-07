063da3d7b3de572a15bc132c84d667a64d91ddaa
Abhijeet Kumar <iamabhijeet.29@gmail.com>
2025-02-07 18:34:34 +0400

    For Opt out of static generation

--- app/api/courses/[courseId]/chapters/[chapterId]/progress/route.ts
+++ app/api/courses/[courseId]/chapters/[chapterId]/progress/route.ts
@@ -2,37 +2,38 @@ import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
+// Opt out of static generation
+export const dynamic = "force-dynamic";
+
 export async function PUT(
-    req: Request,
-    { params }: { params: { courseId: string; chapterId: string; } }
+  req: Request,
+  { params }: { params: { courseId: string; chapterId: string } }
 ) {
-    try {
-
-        const { userId } = auth();
-        if (!userId) {
-            return new NextResponse("Unauthorized", { status: 401 })
-        }
-        const { isCompleted } = await req.json();
-        const userProgress = await db.userProgress.upsert({
-            where: {
-                userId_chapterId: {
-                    userId,
-                    chapterId: params.chapterId
-                }
-            },
-            update: {
-                isCompleted
-            },
-            create: {
-                userId,
-                chapterId: params.chapterId,
-                isCompleted
-            }
-        });
-        return NextResponse.json(userProgress)
-
-    } catch (error) {
-        console.log("[CHAPTER_ID_PROGRESS]", error);
-        return new NextResponse("Internal server error", { status: 500 })
+  try {
+    const { userId } = auth();
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
     }
+    const { isCompleted } = await req.json();
+    const userProgress = await db.userProgress.upsert({
+      where: {
+        userId_chapterId: {
+          userId,
+          chapterId: params.chapterId,
+        },
+      },
+      update: {
+        isCompleted,
+      },
+      create: {
+        userId,
+        chapterId: params.chapterId,
+        isCompleted,
+      },
+    });
+    return NextResponse.json(userProgress);
+  } catch (error) {
+    console.log("[CHAPTER_ID_PROGRESS]", error);
+    return new NextResponse("Internal server error", { status: 500 });
+  }
 }
--- app/api/courses/[courseId]/chapters/[chapterId]/unpublish/route.ts
+++ app/api/courses/[courseId]/chapters/[chapterId]/unpublish/route.ts
@@ -2,62 +2,60 @@ import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
-
-
+// Opt out of static generation
+export const dynamic = "force-dynamic";
 export async function PATCH(
-    req: Request,
-    { params }: { params: { courseId: string, chapterId: string } }
+  req: Request,
+  { params }: { params: { courseId: string; chapterId: string } }
 ) {
-    try {
-        const { userId } = auth();
-        if (!userId) {
-            return new NextResponse("Unauthorized", { status: 401 })
-        }
-
-        const ownCourse = await db.course.findUnique({
-            where: {
-                id: params.courseId,
-                userId
-            }
-        });
-
-        if (!ownCourse) {
-            return new NextResponse("Unauthorized", { status: 401 })
-        };
-
-
-
-        const unpublishedChapter = await db.chapter.update({
-            where: {
-                id: params.chapterId,
-                courseId: params.courseId
-            },
-            data: {
-                isPublished: false
-            }
-        });
-
-        //we need to check whether It has only one chapter, if it has then we need to remove the entire chapter
-
-        const publishedChapterInCourse = await db.chapter.findMany({
-            where: {
-                id: params.courseId,
-                isPublished: true
-            }
-        });
-        if (!publishedChapterInCourse.length) {
-            await db.course.update({
-                where: {
-                    id: params.courseId,
-
-                }, data: {
-                    isPublished: false
-                }
-            })
-        }
-        return NextResponse.json(unpublishedChapter)
-    } catch (error) {
-        console.log("[CHAPTER_UNPUBLISH]", error);
-        return new NextResponse("Internal Server error", { status: 500 })
+  try {
+    const { userId } = auth();
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
     }
+
+    const ownCourse = await db.course.findUnique({
+      where: {
+        id: params.courseId,
+        userId,
+      },
+    });
+
+    if (!ownCourse) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const unpublishedChapter = await db.chapter.update({
+      where: {
+        id: params.chapterId,
+        courseId: params.courseId,
+      },
+      data: {
+        isPublished: false,
+      },
+    });
+
+    //we need to check whether It has only one chapter, if it has then we need to remove the entire chapter
+
+    const publishedChapterInCourse = await db.chapter.findMany({
+      where: {
+        id: params.courseId,
+        isPublished: true,
+      },
+    });
+    if (!publishedChapterInCourse.length) {
+      await db.course.update({
+        where: {
+          id: params.courseId,
+        },
+        data: {
+          isPublished: false,
+        },
+      });
+    }
+    return NextResponse.json(unpublishedChapter);
+  } catch (error) {
+    console.log("[CHAPTER_UNPUBLISH]", error);
+    return new NextResponse("Internal Server error", { status: 500 });
+  }
 }
