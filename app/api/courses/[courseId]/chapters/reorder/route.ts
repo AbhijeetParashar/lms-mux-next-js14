9118ae0de590450d74ac0bc01cb74fd7f482db3e
Abhijeet Kumar <iamabhijeet.29@gmail.com>
2025-02-07 18:47:46 +0400

    For Opt out of static generation

--- app/api/courses/[courseId]/chapters/reorder/route.ts
+++ app/api/courses/[courseId]/chapters/reorder/route.ts
@@ -1,40 +1,39 @@
 import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs/server";
-import { NextResponse } from "next/server"
+import { NextResponse } from "next/server";
 
-
-
+// Opt out of static generation
+export const dynamic = "force-dynamic";
 export async function PUT(
-    req: Request,
-    { params }: { params: { courseId: string } }
+  req: Request,
+  { params }: { params: { courseId: string } }
 ) {
-    try {
-        const { userId } = auth();
-        const { list } = await req.json();
-        if (!userId) {
-            return new NextResponse("Unauthorized", { status: 401 })
-        };
-
-        const ownCourse = await db.course.findUnique({
-            where: {
-                id: params.courseId,
-                userId: userId
-            }
-        });
-        if (!ownCourse) {
-            return new NextResponse("Unauthorized", { status: 401 })
-        }
-        for (let item of list) {
-            await db.chapter.update({
-                where: { id: item.id },
-                data: { position: item.position }
-            })
-        }
-
-        return new NextResponse("Success", { status: 200 })
-
-    } catch (error) {
-        console.log("[ REORDER]", error)
-        return new NextResponse("Internal server error", { status: 500 })
+  try {
+    const { userId } = auth();
+    const { list } = await req.json();
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
     }
+
+    const ownCourse = await db.course.findUnique({
+      where: {
+        id: params.courseId,
+        userId: userId,
+      },
+    });
+    if (!ownCourse) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+    for (let item of list) {
+      await db.chapter.update({
+        where: { id: item.id },
+        data: { position: item.position },
+      });
+    }
+
+    return new NextResponse("Success", { status: 200 });
+  } catch (error) {
+    console.log("[ REORDER]", error);
+    return new NextResponse("Internal server error", { status: 500 });
+  }
 }
