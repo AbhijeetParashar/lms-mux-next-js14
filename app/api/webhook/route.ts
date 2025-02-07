7635ed32614e810b46bdb78dd0e188476a42e022
Abhijeet Kumar <iamabhijeet.29@gmail.com>
2025-02-07 18:50:04 +0400

    For fixing the issue

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -5,41 +5,49 @@ import Stripe from "stripe";
 import { db } from "@/lib/db";
 import { stripe } from "@/lib/stripe";
 
+// Opt out of static generation
+export const dynamic = "force-dynamic";
 
 export async function POST(req: Request) {
-    const body = await req.text();
-    const signature = headers().get("Stripe-Signature") as string;
+  const body = await req.text();
+  const signature = headers().get("Stripe-Signature") as string;
 
-    let event: Stripe.Event;
+  let event: Stripe.Event;
 
-    try {
-        event = stripe.webhooks.constructEvent(
-            body,
-            signature,
-            process.env.STRIPE_WEBHOOK_SECRET!
-        )
-    } catch (error: any) {
-        return new NextResponse(`Webhook Error: ${error?.message}`, { status: 400 })
+  try {
+    event = stripe.webhooks.constructEvent(
+      body,
+      signature,
+      process.env.STRIPE_WEBHOOK_SECRET!
+    );
+  } catch (error: any) {
+    return new NextResponse(`Webhook Error: ${error?.message}`, {
+      status: 400,
+    });
+  }
+
+  const session = event.data.object as Stripe.Checkout.Session;
+  const userId = session?.metadata?.userId;
+  const courseId = session?.metadata?.courseId;
+
+  if (event.type === "checkout.session.completed") {
+    if (!userId || !courseId) {
+      return new NextResponse(`Webhook Error:missing metadata`, {
+        status: 400,
+      });
     }
+    await db.purchase.create({
+      data: {
+        courseId: courseId,
+        userId: userId,
+      },
+    });
+  } else {
+    return new NextResponse(
+      `webhook Error:unhandled event type ${event.type}`,
+      { status: 200 }
+    );
+  }
 
-    const session = event.data.object as Stripe.Checkout.Session;
-    const userId = session?.metadata?.userId;
-    const courseId = session?.metadata?.courseId;
-
-    if (event.type === "checkout.session.completed") {
-        if (!userId || !courseId) {
-            return new NextResponse(`Webhook Error:missing metadata`, { status: 400 })
-        }
-        await db.purchase.create({
-            data: {
-                courseId: courseId,
-                userId: userId
-            }
-        })
-    } else {
-        return new NextResponse(`webhook Error:unhandled event type ${event.type}`, { status: 200 })
-    }
-
-    return new NextResponse(null, { status: 200 })
-
+  return new NextResponse(null, { status: 200 });
 }
